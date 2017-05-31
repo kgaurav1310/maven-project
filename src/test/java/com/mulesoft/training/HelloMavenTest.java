9bc7a58f799234579c2c5bc668796b283a9c2a8a
Kumar Gaurav <kumargaurav@RE-KGAURAV-OSX.T-mobile.com>
2017-05-31 13:41:54 -0400

    Added Test-case-2 to define Content-Type

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
 
@@ -11,16 +12,24 @@ public class HelloMavenTest extends FunctionalTestCase {
 
 	@Rule
 	public DynamicPort dynamicPort = new DynamicPort("http.port");
-	
-    @Test
-    public void mavenFlowReturnsHelloMaven() throws Exception {
-    	System.out.println("\n\nDynamic HTTP port used in Testcase#1-->"+ dynamicPort.getNumber() + "\n\n");
-        runFlowAndExpect("mavenFlow", "Hello Maven");
-    }
-    
-    @Override
-    protected String getConfigFile() {
-        return "maven-project.xml";
-    }
+
+	@Test
+	public void mavenFlowReturnsHelloMaven() throws Exception {
+		System.out.println("\n\nDynamic HTTP port used in Testcase#1-->" + dynamicPort.getNumber() + "\n\n");
+		runFlowAndExpect("mavenFlow", "Hello Maven");
+	}
+
+	@Test
+	public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+		System.out.println("\n\nDynamic HTTP port used in Testcase#2-->" + dynamicPort.getNumber() + "\n\n");
+		MuleEvent event = runFlow("retrieveFlights");
+		String contentType = event.getMessage().getOutboundProperty("Content-Type");
+		assertEquals("application/json", contentType);
+	}
+
+	@Override
+	protected String getConfigFile() {
+		return "maven-project.xml";
+	}
 
 }
